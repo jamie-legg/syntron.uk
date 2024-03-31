2d8481b2c985aa7e901c7c2bebf8a9028a4c7dee
Jamie <legg.jamie@gmail.com>
2024-03-31 23:17:14 +0100

    update start

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run --name TRON -it --rm -p 4534:4534/udp -v syn_uk_settings:/app/server/server codefossa/armagetronad:latest
+docker run --name TRON -it --rm -p 4534:4534/udp -v $(pwd)/syn_uk_settings:/app/server/server codefossa/armagetronad:latest
