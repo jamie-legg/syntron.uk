613afed87e2deaa7d5af159fa83ba2a0eaf0c0df
Jamie <legg.jamie@gmail.com>
2024-03-31 23:15:11 +0100

    name container

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p 4534:4534/udp -v syn_uk_settings:/app/server/server codefossa/armagetronad:latest
+docker run --name TRON -it --rm -p 4534:4534/udp -v syn_uk_settings:/app/server/server codefossa/armagetronad:latest
