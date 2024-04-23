ba5e5b501342e4f8b849a2edd2cdf90dfe805b80
jamie-legg <legg.jamie@gmail.com>
2024-04-23 13:01:44 +0200

    speed insights

--- syntron-web/src/app/layout.tsx
+++ syntron-web/src/app/layout.tsx
@@ -1,16 +1,14 @@
-
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Background from "@/components/Navigation";
-import Navigation from "@/components/Navigation";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Syntron",
-  description: "Your one-stop shop for all things Armagetron Advanced. Downlaod the game, get help, view the leaderboards, contribute to the community, and more!",
+  description:
+    "Your one-stop shop for all things Armagetron Advanced. Downlaod the game, get help, view the leaderboards, contribute to the community, and more!",
   icons: [
     {
       url: "/favicon.ico",
@@ -26,7 +24,8 @@ export default function RootLayout({
   return (
     <html lang="h-full">
       <body className={inter.className + " h-full"}>
-      {children}
+        {children}
+        <SpeedInsights />
       </body>
     </html>
   );
